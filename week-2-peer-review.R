7dfbd0837342943fb79eb3eab1e6ecd63fe1bcd8
Franklin Santos <ssantoss19@hotmail.com>
2020-11-01 16:59:12 -0400

    commit first and second week content of course

---
+++ week-2-peer-review.R
@@ -0,0 +1,39 @@
+#Week 2 Practice Code
+
+#### If you have not installed the tidyverse yet, run the line below. 
+#install.packages("tidyverse")
+
+library(tidyverse)
+
+#####RUN THIS CODE WITHOUT MODIFICATION 
+###This will import the survey data we have been working with in this course and drop data with missing rows from the data
+dat <- drop_na(read.csv(url("https://www.dropbox.com/s/uhfstf6g36ghxwp/cces_sample_coursera.csv?raw=1")))
+
+class(dat)
+vignette("tibble")
+# Problem 1
+
+# The object "dat" created in the assignment code will import the survey data for the assignment using read_csv, thereby creating a tibble. Using that object as your data, use select() to create a new tibble that include only the columns for educational level, whether the respondent has an educational loan, employment status, and Trump approval. Display that object. Hint: consult the codebook to identify the correct column names.
+
+#### Write your code below:
+selectdata <- select(dat, educ, edloan, employ, CC18_308a)
+# Problem 2
+
+# Continuing to use the new data table you created in Problem 1, use recode() to create a new column named "trump_approve_disapprove" that recodes the column for President Trump's job approval. A value of "1" should mean that the respondent either "strongly" or "somewhat" approves of the President, and a value of 0 should mean that the respondent either "strongly" or "somewhat" DISapproves of the president.
+
+
+#### Write your code below:
+newcolum <- mutate(selectdata,
+       trump_approve_disapprove = CC18_308a)
+  
+recodenewcolum <- recode(newcolum$trump_approve_disapprove, `1`=1, `2`=1, `3`=0, `4`=0)
+  
+# Problem 3
+
+# Using the full dataset, use summarise() to create a summary table for survey respondents who are employed full time and are married. The table should have the mean, median and standard deviations for the political viewpoint column.
+
+
+#### Write your code below:
+
+summarise(dat,`Mean Viewpoint`=mean(ideo5), `Median Viewpoint`=median(ideo5))
+
